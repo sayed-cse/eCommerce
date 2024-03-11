826ed7f4a02b10279394aa9fa286034b2b290764
Sayed Zaman <78496742+sayed-cse@users.noreply.github.com>
2024-03-11 19:25:31 +0000

    Delete public directory

--- public/css/css_compressed.php
+++
@@ -1,1 +0,0 @@
-<?php header('Content-type: text/css');ob_start("compress");function compress($buffer){$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);$buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);return $buffer;}include('scss.css');ob_end_flush(); ?>
