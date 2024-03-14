21f65061c95b0f73fc120834446f4c4857fa62e2
Sayed Zaman <78496742+sayed-cse@users.noreply.github.com>
2024-03-14 07:26:59 +0000

    Update BaseModel.php

--- apps/front/models/BaseModel.php
+++ apps/front/models/BaseModel.php
@@ -10,7 +10,14 @@ class BaseModel extends Model
   {
       $this->setSchema("phalconshop");
       #$this->addBehavior(new Timestampable(['beforeCreate' => ['field' => 'created_at', 'format' => 'dmY-His']])); #Dynamic insert/update dateTime in created_at field
-  }   
+  } 
+  public function afterUpdate()
+  {
+      // Call backup function after each successful update
+      $backupFolderPath = BASE_PATH . "/public/schemas"; // Specify the folder where backups will be stored
+      $backup = new \F\Libraries\BackupDB\DbBackup($this->getDI()->get('db'), $backupFolderPath);
+      $backup->backupDatabase();
+  }	
 # Skip NULL Fields No create OR Update By Default
 	// public function beforeValidationOnCreate()
  //    {
